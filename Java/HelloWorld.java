647bdb65b7a3dcc9615be78858dfe6b37efdf86d
mallik8105 <kallem.mallikarjunreddy@gmail.com>
2016-11-27 22:12:21 -0500

    Adding the New line Hello

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,7 +1,8 @@
-public class HelloWorld {
+public class HelloWorld { 	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Hello");
 		System.out.println("namaste peddamanishi");
 	}
 
